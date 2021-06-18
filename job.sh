2c1d2d3344849230eb45fc93a1e6062c84634581
LISPCoC <fourier0520@gmail.com>
2021-06-19 02:40:18 +0900

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 wget https://docs.google.com/spreadsheets/d/1FRi2SYbTpyhduzmoVmI6R06VCvZfuhnkHkqrV5LUXCs/export?format=csv -O mod.csv
 wget https://raw.githubusercontent.com/CleverRaven/Cataclysm-DDA/master/lang/po/ja.po -O ja.po
-python gen.py
+python3 gen.py
 msgcat mod.po ja.po -o cataclysm-dda.po --use-first
 msgfmt cataclysm-dda.po -o cataclysm-dda.mo
 mv cataclysm-dda.* release
