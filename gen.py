27c80c2c0245e20ec22ba16435f76a037939fc7c
LISPCoC <fourier0520@gmail.com>
2021-06-19 02:00:28 +0900

    Update gen.py

--- gen.py
+++ gen.py
@@ -30,7 +30,10 @@ class GenModPo:
             check_for_duplicates = True
         )
 
+        num = 0
         for row in mod_csv:
+            print (num , "/", len(mod_csv))
+            num = num + 1
             # 単数形のみ
             msgctxt = row.get(Const.PO_MSGCTXT)
             if msgctxt == "":
